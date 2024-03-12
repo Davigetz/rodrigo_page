860e3fd401126ec1adca584e4decbb38a04fadd6
Davigetz <Davidstangetz13@gmail.com>
2024-03-12 11:28:51 -0500

    adding nlanding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'raisin-black': '#272837',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
