a3dd523da0077ebee6f7e124faef9124634cce47
David Gomez Rojas <Davidstangetz13@gmail.com>
2024-04-17 22:49:42 -0500

    add spline 3d project

--- src/Pages/Quehacemos.tsx
+++ src/Pages/Quehacemos.tsx
@@ -1,37 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Spline from '@splinetool/react-spline';
 import './quehacemso.css';
 
 const Quehacemos = () => {
   return (
     <div className="que-container">
-      <ul>
-        <li>
-          <p>Diseño de Imagen de identidad total Denominación</p>
-          <b />
-          <p> (nombre, siglah aprocope etc.)</p>
-        </li>
-        <li>
-          <p>Identidad</p>
-          <b />
-          <p>(simbolo, logotipo, diagramación)</p>
-        </li>
-        <li>
-          <p>Branding / Marca Imagen ch productos</p>
-          <b />
-          <p>(Colores corp.)</p>
-        </li>
-        <li>
-          <p>Denominación de Productos textos publicitanos a institucionales.</p>
-          <b />
-          <p>(Manual de Identidad Grafica, Arquitectura corporativa Cachada,</p>
-          <b />
-          <p>edificios negocios, secles, anuncios, vehículos, otos)</p>
-        </li>
-        <p>Asistencia en Registrode Marca R Folletos plegables</p>
-        <b />
-        <p>(Uniformes, pines, corbatas, pañoletas, otos)</p>
-        <li>Diseño Web</li>
-      </ul>
+      <div style={{ width: '100%', height: '70vh', display: 'flex', justifyContent: 'center' }}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline
+            scene="https://draft.spline.design/lenVqiEnsy81YInC/scene.splinecode"
+            style={{ width: '70%' }}
+          />
+        </Suspense>
+      </div>
     </div>
   );
 };
