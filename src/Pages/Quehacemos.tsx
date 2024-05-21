16b12f521766a7859fd18ce187fe9108c8aeb326
David Gomez Rojas <Davidstangetz13@gmail.com>
2024-05-21 18:27:43 -0500

    add todo el entorno 3d

--- src/Pages/Quehacemos.tsx
+++ src/Pages/Quehacemos.tsx
@@ -3,7 +3,7 @@ import { Environment } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
 import gsap from 'gsap';
 import React, { Suspense, useRef, useState } from 'react';
-import { Logo } from '../utils/preload';
+import { Model } from '../utils/preload';
 import './quehacemso.css';
 import { Cloudinary } from '@cloudinary/url-gen';
 const Quehacemos = () => {
@@ -22,7 +22,7 @@ const Quehacemos = () => {
       <div className="flex">
         <div className="h-5/6 w-full flex flex-col justify-center items-center">
           <p id="hero" className="hero-title">
-            Que Hacemos
+            Qué Hacemos
           </p>
           <div className="md:w-10/12 w-9/12 max-w-[2000px] mt-5">
             <video className="pointer-events-none" autoPlay muted playsInline={true}>
@@ -34,15 +34,15 @@ const Quehacemos = () => {
           </div>
         </div>
       </div>
-      {/* <div style={{ width: '100%', height: '70vh', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: '100%', height: '70vh', display: 'flex', justifyContent: 'center' }}>
         <Canvas>
           <Suspense fallback={null}>
-            <Logo />
+            <Model />
             <directionalLight intensity={2} position={[0, 2, 3]} />
             <Environment preset="city" />
           </Suspense>
         </Canvas>
-      </div> */}
+      </div>
     </div>
   );
 };
